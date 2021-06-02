2b11d1f5610fa9f80542ff7cf81736469aecbf1f
likebot <40538763+likebot0@users.noreply.github.com>
2021-06-02 23:27:47 +0900

    initial commit

--- src/bootstrap-compiler/Semantic/Literal/Array.hs
+++ src/bootstrap-compiler/Semantic/Literal/Array.hs
@@ -20,34 +20,37 @@ analyze x = do
         Ast.Node
             do Ast.attributes x
             <$> sequence do
-                Ast.children x >>=
-                    do \(x :: Ast.Node "syntax-analyzed" "discard") -> do
-                        pure $ inject <$> Semantic.Discard.analyze x
-                    @>
-                    do \(x :: Ast.Node "syntax-analyzed" "expression") -> do
-                        pure $ inject <$> Semantic.Expression.analyze x
-                    @>
-                    do \(x :: Ast.Node "syntax-analyzed" "statement/if") -> do
-                        pure $ inject <$> Semantic.Statement.IfStatement.analyze x
-                    @>
-                    do \(x :: Ast.Node "syntax-analyzed" "statement/include") -> do
-                        pure $ inject <$> Semantic.Statement.IncludeStatement.analyze x
-                    @>
-                    do \(x :: Ast.Node "syntax-analyzed" "statement/let") -> do
-                        pure $ inject <$> do
-                            node <- Semantic.Statement.LetStatement.analyze x
+                fmap
+                    (
+                        do \(x :: Ast.Node "syntax-analyzed" "discard") ->
+                            inject <$> Semantic.Discard.analyze x
+                        @>
+                        do \(x :: Ast.Node "syntax-analyzed" "expression") ->
+                            inject <$> Semantic.Expression.analyze x
+                        @>
+                        do \(x :: Ast.Node "syntax-analyzed" "statement/if") ->
+                            inject <$> Semantic.Statement.IfStatement.analyze x
+                        @>
+                        do \(x :: Ast.Node "syntax-analyzed" "statement/include") ->
+                            inject <$> Semantic.Statement.IncludeStatement.analyze x
+                        @>
+                        do \(x :: Ast.Node "syntax-analyzed" "statement/let") ->
+                            inject <$> do
+                                node <- Semantic.Statement.LetStatement.analyze x
 
-                            let (identifierNode, expressionNode) = Ast.children $ Ast.children node
+                                let (identifierNode, expressionNode) = Ast.children $ Ast.children node
 
-                            let identifier = Ast.children identifierNode
+                                let identifier = Ast.children identifierNode
 
-                            memberStore <- get memberStoreRef
+                                memberStore <- get memberStoreRef
 
-                            set memberStoreRef $ Data.HashMap.Strict.insert identifier expressionNode memberStore
+                                set memberStoreRef $ Data.HashMap.Strict.insert identifier expressionNode memberStore
 
-                            pure node
-                    @>
-                    do \(x :: Ast.Node "syntax-analyzed" "statement/with") -> do
-                        pure $ inject <$> Semantic.Statement.WithStatement.analyze x
-                    @>
-                    typesExhausted
+                                pure node
+                        @>
+                        do \(x :: Ast.Node "syntax-analyzed" "statement/with") ->
+                            inject <$> Semantic.Statement.WithStatement.analyze x
+                        @>
+                        typesExhausted
+                    )
+                    do Ast.children x 
